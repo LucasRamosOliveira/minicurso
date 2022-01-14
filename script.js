616e332e1f40c5a04839398e74ed0c6fb3f95fed
Lucas Oliveira <lucas.rmo.15.si@gmail.com>
2022-01-14 19:03:56 -0300

    Corrigir html

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const imagen = document.querySelector('img');
+const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const nomeDoPersonagem = document.querySelector('#especie');
-const nomeDoPersonagem = document.querySelector('#status');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": "Application/json"
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.especie;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
